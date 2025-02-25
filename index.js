2878ec06e51ea32b385ea2d8dad5d5bf511ee40e
Robert Malikov <Robert.Malikov@urfu.me>
2025-02-25 19:18:16 +0500

    Маликов Роберт

--- index.js
+++ index.js
@@ -4,36 +4,139 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let nextSymbol = CROSS;
+let turnsLeft;
+let endGame = false;
+let dimensionGlobal;
+
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    let dim = prompt("Размер сетки", 3);
+    if (dim < 3 || dim === undefined) {
+        alert("Сликшом мало! Делаем сетку из 3");
+        dim = 3;
+    }
+    renderGrid(Number(dim));
+    
 }
 
 function renderGrid (dimension) {
+    dimensionGlobal = dimension;
+    let gridData = [];
+    turnsLeft = dimension ** 2;
     container.innerHTML = '';
-
+    
     for (let i = 0; i < dimension; i++) {
+        gridData.push([]);
         const row = document.createElement('tr');
         for (let j = 0; j < dimension; j++) {
+            gridData[i].push(EMPTY);
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
-            cell.addEventListener('click', () => cellClickHandler(i, j));
+            cell.addEventListener('click', () => cellClickHandler(gridData, i, j));
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
+
+function cellClickHandler (gridData, row, col) {
+    
+    if (!endGame && gridData[row][col] === EMPTY) {
+        renderSymbolInCell(nextSymbol, row, col);
+        gridData[row][col] = nextSymbol;
+
+        if (isWinner(gridData, nextSymbol)) {
+            setTimeout(alert, 0.1, `${nextSymbol} win`);
+            endGame = true;
+        }
+
+
+        nextSymbol = nextSymbol === CROSS ? ZERO : CROSS;
+        turnsLeft--;
+
+        if (turnsLeft == 0) {
+            setTimeout(alert, 0.1, "Победила дружба");
+            endGame = true;
+        }
+    }
+    
     console.log(`Clicked on cell: ${row}, ${col}`);
+}
 
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function isWinner (gridData, player) {
+    let winCells = []; 
+    
+    let horizontalWin = 0;
+    for (let row = 0; row < dimensionGlobal; row++) {
+        for (let col = 0; col < dimensionGlobal; col++) {
+            if (gridData[row][col] === player) {
+                horizontalWin++;
+                winCells.push([row, col]);
+                if (horizontalWin === dimensionGlobal) {
+                    paintCells(player, winCells);
+                    return true;
+                }
+            }
+        }
+        winCells = [];
+        horizontalWin = 0;
+    }
+
+    let verticalWin = 0;
+    for (let i = 0; i < dimensionGlobal; i++) {
+        for (let j = 0; j < dimensionGlobal; j++) {
+            if (gridData[j][i] === player) {
+                verticalWin++;
+                winCells.push([j, i]);
+            }
+            if (verticalWin === dimensionGlobal) {
+                paintCells(player, winCells);
+                return true;
+            }
+        }   
+        winCells = [];
+        verticalWin = 0;
+    }
+
+    let diagonalWin = 0;
+    for (let i = 0; i < dimensionGlobal; i++) {
+        if (gridData[i][i] === player) {
+            diagonalWin++;
+            winCells.push([i, i]);
+        }
+    }
+    if (diagonalWin === dimensionGlobal) {
+        paintCells(player, winCells);
+        return true;
+    }
+    winCells = [];
+    diagonalWin = 0;
+
+    for (let i = 0; i < dimensionGlobal; i++) {
+        for (let j = 0; j < dimensionGlobal; j++) {
+            if (i + j === dimensionGlobal - 1 && gridData[i][j] == player) {
+                winCells.push([i, j]);
+                diagonalWin++;
+            }
+        }
+    }
+    if (diagonalWin === dimensionGlobal) {
+        paintCells(player, winCells);
+        return true;
+    }
+
+    return false;
+}
+
+function paintCells(player, winCells) {
+    for (const cell of winCells) {
+        renderSymbolInCell(player, cell[0], cell[1], "#FF0000");
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,7 +157,8 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
-    console.log('reset!');
+    endGame = false;
+    startGame();
 }
 
 
